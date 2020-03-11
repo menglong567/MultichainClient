910027c308fb2172c6b3c35dfcbde6b03a925f22
leo <menglong567@aliyun.com>
2020-03-11 22:00:31 +0800

    add swagger2 support

--- src/main/java/com/ml/multichain/client/server/MultichainClientServer.java
+++ src/main/java/com/ml/multichain/client/server/MultichainClientServer.java
@@ -3,11 +3,13 @@ package com.ml.multichain.client.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author mengl
  */
 @SpringBootApplication(scanBasePackages = "com.ml")
+@EnableSwagger2
 public class MultichainClientServer {
     public static void main(String[] args) {
         SpringApplication.run(MultichainClientServer.class, args);
